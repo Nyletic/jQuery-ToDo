0619da3392eae9c4e94ca5542671ed455d67f5e0
Nyletic <nikolicmihael1@gmail.com>
2023-12-01 22:29:41 +0100

    todo

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function () {
+  $("#input").change(function () {
+    let input = $(this).val();
+    $("ul").append(`<li>${input} <i class="fa-solid fa-check"></i> 
+    <i class="fa-solid fa-trash"></i></li>`);
+    $(this).val("");
+  });
+});
+
+$("ul").on("click", ".fa-trash", function () {
+  $(this).parent("li").fadeOut(300);
+});
+
+$("ul").on("click", ".fa-check", function () {
+  $(this).parent("li").toggleClass("checked");
+});
